fb2031d468a5812635fc9e3572b1648ed65cf0b5
Tivra Raj <tivraraj19@gmail.com>
2023-12-25 20:19:36 +0530

    Created UI, Timer, Interactablr and GameOver system

---
+++ UnityDeveloper_Test/Assets/Scripts/Interactables/PointCubeView.cs
@@ -0,0 +1,15 @@
+﻿using Services;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class PointCubeView : MonoBehaviour, IInteractable
+    {
+        public void Interact()
+        {
+            int currentPoint = GameService.Instance.GetPlayerController().TotalPointCubeCollected;
+            EventService.Instance.OnPoinCubeColletedEvent.InvokeEvent(++currentPoint);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ UnityDeveloper_Test/Assets/Scripts/Services/EventService.cs
@@ -0,0 +1,27 @@
+﻿using Event;
+
+namespace Services
+{
+    public class EventService
+    {
+        private static EventService instance;
+        public static EventService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EventService();
+                }
+                return instance;
+            }
+        }
+
+        public EventController<int> OnPoinCubeColletedEvent { get; private set; }
+
+        public EventService()
+        {
+            OnPoinCubeColletedEvent = new EventController<int>();
+        }
+    }
+}
